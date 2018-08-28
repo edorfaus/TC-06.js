b937023675d26abe8a7cc2bcdb8038358281cbc3
Frode Austvik <edorfaus@xepher.net>
2018-08-28 22:48:01 +0200

    Added a very simple event emitter to the RAM class.

--- devices/ram.js
+++ devices/ram.js
@@ -12,6 +12,7 @@ class RAM
 		this._maxAddress = Math.pow(2, addressBits) - 1;
 		this._maxValue = Math.pow(2, dataBits) - 1;
 		this._data = new Array(this._maxAddress + 1).fill(0);
+		this._listeners = [];
 	}
 	read(address) {
 		if (address < 0 || address > this._maxAddress) {
@@ -26,7 +27,24 @@ class RAM
 		if (value < 0 || value > this._maxValue) {
 			throw new Error('RAM write failed: data value out of range');
 		}
+		let oldValue = this._data[address];
 		this._data[address] = value;
+		if (value !== oldValue && this._listeners.length != 0) {
+			let event = {
+				instance: this,
+				address: address,
+				oldValue: oldValue,
+				newValue: value
+			};
+			for (let listener of this._listeners) {
+				listener.call(this, event);
+			}
+		}
 		return true;
+	}
+	on(event, listener) {
+		if (event === 'change') {
+			this._listeners.push(listener);
+		}
 	}
 }
