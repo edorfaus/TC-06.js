04adbdc9d2954e9048346c9acff006ab20b4a3c5
Frode Austvik <edorfaus@xepher.net>
2018-10-15 02:30:16 +0200

    Refactored the Screen class to have the video memory passed in.

--- devices/screen.js
+++ devices/screen.js
@@ -1,18 +1,29 @@
 class Screen
 {
-	constructor(colorBits, xBits, yBits) {
+	constructor(colorBits, xBits, yBits, totalBits, videoMemory) {
 		if (colorBits < 1 || xBits < 1 || yBits < 1) {
-			throw new Error('Invalid arguments for screen: bit count below 1');
+			throw new Error('Invalid screen arguments: bit count below 1');
 		}
-		if (colorBits + xBits + yBits > 32) {
-			throw new Error('Invalid arguments for screen: total bits > 32');
+		if (colorBits + xBits + yBits > totalBits) {
+			throw new Error('Invalid screen arguments: bit sum > total bits');
+		}
+		if (totalBits > 32) {
+			// Due to the way we use JS bitwise operations, 32 bits is max.
+			throw new Error('Invalid screen arguments: total bits > 32');
+		}
+		if (Math.pow(2, totalBits) > videoMemory.valueRange.size) {
+			throw new Error('Invalid screen arguments: memory values too small');
+		}
+		if (Math.pow(2, xBits + yBits) > videoMemory.addressRange.size) {
+			throw new Error('Invalid screen arguments: memory size too small');
 		}
 		this._colorBits = colorBits;
 		this._xBits = xBits;
 		this._yBits = yBits;
-		this._shift = 32 - colorBits - xBits - yBits;
+		this._shift = totalBits - colorBits - xBits - yBits;
 		this._mask = Math.pow(2, xBits + yBits) - 1;
-		this._videoMemory = new RAM(xBits + yBits, 32);
+		this._videoMemory = videoMemory;
+
 	}
 	get videoMemory() {
 		return this._videoMemory;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let deviceBus = new DeviceBus(4, 32);
 
 memoryBus.attachDevice(0, 32, new RAM(5, 32));
 
-let screen = new Screen(2, 4, 3);
+let screen = new Screen(2, 4, 3, 32, new RAM(4 + 3, 32));
 deviceBus.attachDevice(0, 1, screen);
 deviceBus.attachDevice(1, 1, new Drive(new RAM(8, 32)));
 
