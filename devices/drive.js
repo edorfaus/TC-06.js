6217c1fa275e5466ca97c9ad44aa1b32b3154311
Frode Austvik <edorfaus@xepher.net>
2018-10-16 08:14:30 +0200

    Changed the Drive to use the memory's ranges instead of extra checks.

--- devices/drive.js
+++ devices/drive.js
@@ -2,32 +2,18 @@ class Drive
 {
 	constructor(memory) {
 		this._memory = memory;
-		this._maxAddress = memory.maxAddress;
-		this._maxValue = memory.maxValue;
 		this._writeAddress = null;
 	}
 	getData(address) {
-		if (address < 0 || address > this._maxAddress) {
-			// Read failed: address out of range
-			return;
-		}
 		return this._memory.read(address);
 	}
 	setData(data) {
 		if (this._writeAddress === null) {
-			if (data < 0 || address > this._maxAddress) {
-				// Write failed: address out of range
-				return false;
-			}
-			this._writeAddress = data;
+			this._writeAddress = this._memory.addressRange.fix(data);
 			return true;
 		}
 		let address = this._writeAddress;
 		this._writeAddress = null;
-		if (value < 0 || value > this._maxValue) {
-			// Write failed: data out of range
-			return false;
-		}
 		return this._memory.write(address, value);
 	}
 }
